ea1f2316f74d6cb7b916f569b8aa7dff925257a9
MikalaiF <filmikalai@gmail.com>
2023-04-15 21:20:20 +0300

    feat: completed task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   constructor(flag = true) {
+      this.reverseflag = flag;
+   }
+   encrypt(string, key) {
+      if (string === undefined || key === undefined) {
+         throw new Error('Incorrect arguments!')
+      }
+      let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let bigStr = string.toUpperCase();
+      let result = [];
+      let encryption = [];
+      let stackShifr = key.split('');
+
+      for (let i = 0; i < string.length; i++) {
+         if (string[i] === ' ') {
+            encryption.push(' ');
+            stackShifr.unshift('!');
+         } else {
+            if (stackShifr[i] === undefined) {
+               stackShifr = key.split('');
+               string = string.slice(i);
+               i = 0;
+            }
+            encryption.push(stackShifr[i].toUpperCase());
+         }
+      }
+      for (let i = 0; i < bigStr.length; i++) {
+         let index = 0;
+         if (abc.indexOf(bigStr[i]) > -1) {
+            if ((abc.indexOf(bigStr[i]) + abc.indexOf(encryption[i])) > abc.length - 1) {
+               index = Math.abs(abc.length - (abc.indexOf(bigStr[i]) + abc.indexOf(encryption[i])));
+            } else {
+               index = abc.indexOf(bigStr[i]) + abc.indexOf(encryption[i]);
+            }
+            result.push(abc[index]);
+         } else {
+            result.push(bigStr[i]);
+         }
+      }
+      if (this.reverseflag === false) {
+         return result.reverse().join('');
+      } else return result.join('');
+   }
+   decrypt(string, key) {
+      if (string === undefined || key === undefined) {
+         throw new Error('Incorrect arguments!')
+      }
+      let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let bigStr = string.toUpperCase();
+      let result = [];
+
+      let encryption = [];
+      let stackShifr = key.split('');
+      for (let i = 0; i < string.length; i++) {
+         if (string[i] === ' ') {
+            encryption.push(' ');
+            stackShifr.unshift('!');
+         } else {
+            if (stackShifr[i] === undefined) {
+               stackShifr = key.split('');
+               string = string.slice(i);
+               i = 0;
+            }
+            encryption.push(stackShifr[i].toUpperCase());
+         }
+      }
+      for (let i = 0; i < bigStr.length; i++) {
+         let index = 0;
+         if (abc.indexOf(bigStr[i]) > -1) {
+            if ((abc.indexOf(bigStr[i]) < abc.indexOf(encryption[i]))) {
+               index = Math.abs(abc.length + (abc.indexOf(bigStr[i]) - abc.indexOf(encryption[i])));
+            } else {
+               index = abc.indexOf(bigStr[i]) - abc.indexOf(encryption[i]);
+            }
+            result.push(abc[index]);
+         } else {
+            result.push(bigStr[i]);
+         }
+      }
+      if (this.reverseflag === false) {
+         return result.reverse().join('');
+      } else return result.join('');
+   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+   VigenereCipheringMachine
 };
